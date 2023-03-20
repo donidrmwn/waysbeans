9a300172fab02dd22547bfabfb2196b4866bc6d8
donidarmawan <44544869+zuro64@users.noreply.github.com>
2023-03-20 07:37:16 +0700

    dsacdwadc

--- server/repositories/transaction.go
+++ server/repositories/transaction.go
@@ -106,7 +106,7 @@ func (r *repository) FindTransactionsByProductName(userID int, productName strin
 	var transactions []models.Transaction
 	searchProductName := "%" + productName + "%"
 
-	productsId := r.db.Select("id").Where("name like ? ", searchProductName).Table("products")
+	productsId := r.db.Select("id").Where("name ilike ? ", searchProductName).Table("products")
 	fmt.Println(productName)
 	fmt.Println(productsId)
 	transactionsId := r.db.Select("transaction_id").Where("product_id in (?)", productsId).Table("carts")
